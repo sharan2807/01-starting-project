ab1c168ad454faf9f98b4782fb8f817bb636e33e
sharan2807 <sharancold98@gmail.com>
2023-12-25 15:56:28 +0530

    Resetting i/p boxes to empty after form submission

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,30 +1,41 @@
 import React, { useState } from "react";
 import Card from "../UI/Card";
-import classes from './AddUser.module.css'
+import classes from "./AddUser.module.css";
 import Button from "../UI/Button";
 
-
 const AddUser = (props) => {
-  const[enteredUsername,setEnteredUsername]=useState('')
-  const[enteredAge,setEnteredAge]=useState('')
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
-  const usernameChangeHandler=(event)=>{
-    setEnteredUsername(event.target.value)
-  }
-  const ageChangeHandler=(event)=>{
-    setEnteredAge(event.target.value)
-  }
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler}/>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
