433bb5d222c3398ae030ad53c2a0ae5c0bf7a18a
sharan2807 <sharancold98@gmail.com>
2023-12-26 15:07:15 +0530

    Adding proper error messages according to problem

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,14 +7,23 @@ import ErrorModal from "../UI/ErrorModal";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const[error,setError]=useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: 'Please enter a valid name and age (non-empty values).',
+      })
       return;
     }
     if (enteredAge < 1) {
+      setError({
+        title: "Invalid Age",
+        message: "Please enter a valid age ( Age >0 )",
+      })
       return;
     }
     // console.log(enteredUsername, enteredAge);
@@ -30,7 +39,7 @@ const AddUser = (props) => {
   };
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
